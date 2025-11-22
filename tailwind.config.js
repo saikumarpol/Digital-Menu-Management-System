8c3ead520681edbf470d211aba5f62f58b619843
saikumarpol <saikumarpola4@gmail.com>
2025-11-22 11:32:42 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx,css}',
-    './src/styles/**/*.{css}'
+    './src/styles/**/*.css'
   ],
   theme: {
     extend: {},
